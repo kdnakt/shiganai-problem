2939ea10b0e285af68fa3cf43b29fab8275b5f04
kdnakt <a.kid.1985@gmail.com>
2018-01-13 15:37:37 +0900

    implement run

--- java/src/main/java/org/shiganai/shiganai_problem/App.java
+++ java/src/main/java/org/shiganai/shiganai_problem/App.java
@@ -9,6 +9,8 @@ public class App {
     static final int LOOP_COUNT = 100;
     static final String CHARS = "しくてがない";
     static final String TARGET = "しがない";
+    static final String RADIO_MSG = "第%s回SIerのSEからWEB系のエンジニアに転職したが楽しくて仕方がないラジオ、略して「しがないラジオ」";
+    static final String NON_RADIO_MSG = "「%sラジオ」ちゃうやん";
 
     public static void main(final String[] args) {
         final App app = new App();
@@ -16,8 +18,19 @@ public class App {
     }
 
     public void run() {
+        int n = -1;
+        String word = null;
         for (int i = 0; i < LOOP_COUNT; i++) {
-            final String word = makeRandomWord();
+            word = makeRandomWord();
+            if (TARGET.equals(word)) {
+                n = i + 1;
+                break;
+            }
+        }
+        if (n > 0) {
+            System.out.println(String.format(RADIO_MSG, n));
+        } else {
+            System.out.println(String.format(NON_RADIO_MSG, word));
         }
     }
 
