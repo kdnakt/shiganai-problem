134fb7be6dd0afdc9fbeb4875efecc99a0e8a9bd
kdnakt <a.kid.1985@gmail.com>
2018-01-13 13:21:48 +0900

    add makeRandomWord

---
+++ java/src/main/java/org/shiganai/shiganai_problem/App.java
@@ -0,0 +1,26 @@
+package org.shiganai.shiganai_problem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	static final int LOOP_COUNT = 100;
+	static final String CHARS = "しくてがない";
+	static final String TARGET = "しがない";
+	
+	public static void main(final String[] args)	{
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			final String word = makeRandomWord();
+			System.out.println(word);
+			
+		}
+	}
+
+	static String makeRandomWord() {
+		// TODO Auto-generated method stub
+		return TARGET;
+	}
+
+}
---
+++ java/src/test/java/org/shiganai/shiganai_problem/AppTest.java
@@ -0,0 +1,34 @@
+package org.shiganai.shiganai_problem;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName ) {
+        super( testName );    
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testMakeRandomWord() {
+    	    final String word = App.makeRandomWord();
+        assertTrue(word.length() == App.TARGET.length());
+        
+        // assert word is random
+    }
+}
