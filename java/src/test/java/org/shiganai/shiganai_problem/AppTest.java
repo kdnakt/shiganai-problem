40d4bb9f9b061e16add330de337831f8c40976b2
kdnakt <a.kid.1985@gmail.com>
2018-01-13 13:37:49 +0900

    fix testMakeRandomWord

--- java/src/test/java/org/shiganai/shiganai_problem/AppTest.java
+++ java/src/test/java/org/shiganai/shiganai_problem/AppTest.java
@@ -30,5 +30,21 @@ public class AppTest extends TestCase {
         assertTrue(word.length() == App.TARGET.length());
         
         // assert word is random
+        int i = 0;
+        final char c1 = word.charAt(i++);
+        final char c2 = word.charAt(i++);
+        final char c3 = word.charAt(i++);
+        final char c4 = word.charAt(i++);
+
+        assertTrue(App.CHARS.indexOf(c1) > -1);
+        assertTrue(App.CHARS.indexOf(c2) > -1);
+        assertTrue(App.CHARS.indexOf(c3) > -1);
+        assertTrue(App.CHARS.indexOf(c4) > -1);
+
+        assertTrue(c1 != c2);
+        assertTrue(c1 != c3);
+        assertTrue(c1 != c4);
+        assertTrue(c2 != c3);
+        assertTrue(c2 != c4);
     }
 }
