13c97bfb44b63887e74fe903d94d6a560a47e922
kdnakt <a.kid.1985@gmail.com>
2018-01-13 15:28:24 +0900

    implement makeRandomWord

--- java/src/main/java/org/shiganai/shiganai_problem/App.java
+++ java/src/main/java/org/shiganai/shiganai_problem/App.java
@@ -1,26 +1,32 @@
 package org.shiganai.shiganai_problem;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
 
     static final int LOOP_COUNT = 100;
     static final String CHARS = "しくてがない";
     static final String TARGET = "しがない";
 
-    public static void main(final String[] args)    {
+    public static void main(final String[] args) {
+        final App app = new App();
+        app.run();
+    }
+
+    public void run() {
         for (int i = 0; i < LOOP_COUNT; i++) {
             final String word = makeRandomWord();
-            System.out.println(word);
-            
         }
     }
 
-    static String makeRandomWord() {
-        // TODO Auto-generated method stub
-        return TARGET;
+    String makeRandomWord() {
+        final List<String> list = Arrays.asList(CHARS.split(""));
+        Collections.shuffle(list);
+        int i = 0;
+        return new StringBuilder(list.get(i++)).append(list.get(i++)).append(list.get(i++)).append(list.get(i++))
+                .toString();
     }
 
 }
--- java/src/test/java/org/shiganai/shiganai_problem/AppTest.java
+++ java/src/test/java/org/shiganai/shiganai_problem/AppTest.java
@@ -1,33 +1,22 @@
 package org.shiganai.shiganai_problem;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName ) {
-        super( testName );    
-    }
+import mockit.integration.junit4.JMockit;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite( AppTest.class );
-    }
+@RunWith(JMockit.class)
+public class AppTest {
 
+    @Test
     public void testMakeRandomWord() {
-        final String word = App.makeRandomWord();
+        final App app = new App();
+        final String word = app.makeRandomWord();
         assertTrue(word.length() == App.TARGET.length());
+        assertTrue(StringUtils.containsOnly(word, App.CHARS));
 
         // assert word is random
         int i = 0;
@@ -35,11 +24,6 @@ public class AppTest extends TestCase {
         final char c2 = word.charAt(i++);
         final char c3 = word.charAt(i++);
         final char c4 = word.charAt(i++);
-
-        assertTrue(App.CHARS.indexOf(c1) > -1);
-        assertTrue(App.CHARS.indexOf(c2) > -1);
-        assertTrue(App.CHARS.indexOf(c3) > -1);
-        assertTrue(App.CHARS.indexOf(c4) > -1);
 
         assertTrue(c1 != c2);
         assertTrue(c1 != c3);
