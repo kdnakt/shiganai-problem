7342184727320333d2d0149f5099099b710d971c
kdnakt <a.kid.1985@gmail.com>
2018-04-21 13:52:14 +0900

    show fail message

--- python/shiganai.py
+++ python/shiganai.py
@@ -9,9 +9,11 @@ def shiganai():
     for i in range(100):
         res = ''.join(random.sample(params, len(params)))
         test = res[0] + res[1] + res[2] + res[3]
-        if test == target:
+        result = test == target
+        if result:
             print("第{}回SIerのSEからWEB系のエンジニアに転職したが楽しくて仕方がないラジオ、略して「しがないラジオ」".format(i + 1))
             break
-    print("end")
+    if not result:
+        print("「{}ラジオ」ちゃうやん".format(test.encode("utf-8")))
 
 shiganai()
