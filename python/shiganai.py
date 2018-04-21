e5f64c06ab5ca4d4d1d5cee16c7417bccee02fa6
kdnakt <a.kid.1985@gmail.com>
2018-04-21 13:45:55 +0900

    show shiganai radio

--- python/shiganai.py
+++ python/shiganai.py
@@ -10,7 +10,7 @@ def shiganai():
         res = ''.join(random.sample(params, len(params)))
         test = res[0] + res[1] + res[2] + res[3]
         if test == target:
-            print(test)
+            print("第{}回SIerのSEからWEB系のエンジニアに転職したが楽しくて仕方がないラジオ、略して「しがないラジオ」".format(i + 1))
             break
     print("end")
 
