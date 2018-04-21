9f320a1d91693aac83c540794c6cea13d5192b9b
kdnakt <a.kid.1985@gmail.com>
2018-04-21 13:42:43 +0900

    fix shiganai check

--- python/shiganai.py
+++ python/shiganai.py
@@ -3,13 +3,13 @@
 import random
 
 params = u"しくてがない"
-shiganai = "しがない"
+target = u"しがない"
 
 def shiganai():
     for i in range(100):
         res = ''.join(random.sample(params, len(params)))
         test = res[0] + res[1] + res[2] + res[3]
-        if test == shiganai:
+        if test == target:
             print(test)
             break
     print("end")
