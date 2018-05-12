7c2091c7f42a95d1c7a4a74600e14f58599f8413
kdnakt <a.kid.1985@gmail.com>
2018-05-12 18:13:42 +0900

    cpp: shuffle original string

--- cpp/shiganai.cpp
+++ cpp/shiganai.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
+#include <random>
 
 using namespace std;
 
+void shuffle(string array[], int size) {
+  random_device rnd;
+  for (int i = 0; i < size; i++) {
+    int j = rnd() % size;
+    string temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 int main() {
-  const string sample = "しくてがない";
-  cout << sample << "\n";
+  string sample[6] = {"し", "く", "て", "が", "な", "い"};
+  shuffle(sample, 6);
+  cout << sample[0] << sample[1] << sample[2] << sample[3] << "\n";
   return 0;
 }
