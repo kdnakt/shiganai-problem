4af0c651216408c1206507805c56b516dac48d00
kdnakt <a.kid.1985@gmail.com>
2018-06-20 08:36:08 +0900

    Vue.js: initial commit

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <ShiganaiProblem msg="Click Try Button!"/>
+  </div>
+</template>
+
+<script>
+import ShiganaiProblem from './components/ShiganaiProblem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ShiganaiProblem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/src/components/ShiganaiProblem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="getResult">Try</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShiganaiProblem',
+  props: {
+    msg: String
+  },
+  methods: {
+    getResult: function() {
+      const LOOP_COUNT = 100;
+      const CHARS = "しくてがない";
+      const TARGET = "しがない";
+
+      function getRandomWord() {
+        const arr = CHARS.split("");
+        let clen = CHARS.length;
+        var word = "";
+        for (let i = 0, len = TARGET.length; i < len; i++) {
+          let n = Math.floor(Math.random() * clen--);
+          word += arr[n];
+          arr.splice(n, 1);
+        }
+        return word;
+      }
+
+      function shiganai() {
+        let i = 0;
+        let msg, title;
+        for (; i < LOOP_COUNT; i++) {
+          title = getRandomWord();
+          if (title == TARGET) {
+            msg = `第${i + 1}回SIerのSEからWEB系のエンジニアに転職したが楽しくて仕方がないラジオ、略して「しがないラジオ」`;
+            break;
+          }
+        }
+        if (msg) {
+          return msg;
+        } else {
+          return `「${title}ラジオ」ちゃうやん`;
+        }
+      }
+
+      this.msg = shiganai();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  word-wrap: break-word;
+  width: 50%;
+  margin: 8px auto;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
