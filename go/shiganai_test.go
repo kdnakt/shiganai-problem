853b925734d59fbd42960a2604394175e7c8285d
kdnakt <a.kid.1985@gmail.com>
2018-08-24 09:23:58 +0900

    go: add testacses

--- go/shiganai_test.go
+++ go/shiganai_test.go
@@ -4,10 +4,47 @@ import (
 	"testing"
 )
 
-func TestProduce(t *testing.T) {
-	sp := &ShiganaiProblem{10}
-	produced := sp.Produce()
-	if len(produced) != 4 * 3 {
+func TestShiganaiProducer(t *testing.T) {
+	p := &ShiganaiProducer{}
+	produced := p.Produce()
+	if len(produced) != 4*3 {
 		t.Fatal("produced illegal value")
+	}
+}
+
+type FalseProducer struct {
+}
+
+func (p *FalseProducer) Produce() string {
+	return "しながい"
+}
+
+func TestFalseProfucer(t *testing.T) {
+	fp := &FalseProducer{}
+	sp := &ShiganaiProblem{10, fp}
+	result := sp.Result()
+	if result != "「しながいラジオ」ちゃうやん" {
+		t.Fatal("Wrong result")
+	}
+}
+
+type ThirdProducer struct {
+	count int
+}
+
+func (p *ThirdProducer) Produce() string {
+	p.count++
+	if p.count == 3 {
+		return "しがない"
+	}
+	return "しながい"
+}
+
+func TestThirdProducer(t *testing.T) {
+	tp := &ThirdProducer{}
+	sp := &ShiganaiProblem{10, tp}
+	result := sp.Result()
+	if result != "第3回SIerのSEからWEB系のエンジニアに転職したが楽しくて仕方がないラジオ、略して「しがないラジオ」" {
+		t.Fatal(result)
 	}
 }
